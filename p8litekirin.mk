78c5f95eab78f2796f4ddcda6f066ce8644e467f
Hakam El Assouad <hakamelassouad@gmail.com>
2016-09-14 01:18:07 +0200

    hi6210sft: rename file

--- p8litekirin.mk
+++ p8litekirin.mk
@@ -7,7 +7,7 @@ PRODUCT_CHARACTERISTICS := default
 
 # Charger
 PRODUCT_PACKAGES += \
-		charger_res_images
+	charger_res_images
 		
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
@@ -18,37 +18,37 @@ TARGET_SCREEN_WIDTH := 720
 
 # File System
 PRODUCT_PACKAGES += \
-		make_ext4fs \
-		setup_fs
+	make_ext4fs \
+	setup_fs
 
 # GPS
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
-		debug.hwui.render_dirty_regions=false \
-		hw.lcd.lcd_density=320 \		
-		persist.sys.strictmode.disable=1 \
-		persist.sys.use_dithering=2 \
-		ro.opengles.version=131072 \
-		ro.sf.lcd_density=320
+	debug.hwui.render_dirty_regions=false \
+	hw.lcd.lcd_density=320 \		
+	persist.sys.strictmode.disable=1 \
+	persist.sys.use_dithering=2 \
+	ro.opengles.version=131072 \
+	ro.sf.lcd_density=320
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
-		LiveWallpapersPicker \
-		librs_jni
+	LiveWallpapersPicker \
+	librs_jni
 
 # Wifi
 PRODUCT_PACKAGES := \
-		libwpa_client \
-		wpa_supplicant
+	libwpa_client \
+	wpa_supplicant
 		
 # Zygote
 ADDITIONAL_DEFAULT_PROPERTIES += \
 	ro.zygote=zygote64_32
 	
 PRODUCT_COPY_FILES += \
-		system/core/rootdir/init.zygote64_32.rc:root/init.zygote64_32.rc
+	system/core/rootdir/init.zygote64_32.rc:root/init.zygote64_32.rc
 	
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 	ro.zygote=zygote64_32
