7c6cc386c18393168f778421e0b7c12ba58e18a0
Hakam El Assouad <hakamelassouad@gmail.com>
2016-09-14 01:36:50 +0200

    hi6210sft: add PRODUCT_RELEASE_NAME

--- cm.mk
+++ cm.mk
@@ -2,11 +2,13 @@
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
+PRODUCT_RELEASE_NAME := p8litekirin
+
 # Device
 $(call inherit-product, device/huawei/p8litekirin/p8litekirin.mk)
 
 PRODUCT_DEVICE := p8litekirin
 PRODUCT_NAME := cm_p8litekirin
-PRODUCT_BRAND := huawei
-PRODUCT_MODEL := P8 Lite
-PRODUCT_MANUFACTURER := HUAWEI
+PRODUCT_BRAND := Huawei
+PRODUCT_MODEL := ALE-L21
+PRODUCT_MANUFACTURER := Huawei
