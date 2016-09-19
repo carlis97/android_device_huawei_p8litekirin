e639d0d6cfe0091ce4c8ce03fb5c2cc5101251ec
hak86 <hakamelassouad@gmail.com>
2016-09-20 00:35:14 +0200

    hi6210sft: clean up

--- Android.mk
+++
@@ -1,7 +0,0 @@
-ifneq ($(filter p8litekirin, $(TARGET_DEVICE)),)
-
-LOCAL_PATH := $(call my-dir)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
-
-endif
--- AndroidProducts.mk
+++
@@ -1,3 +0,0 @@
-PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/aosp_p8litekirin.mk \
-	$(LOCAL_DIR)/full_p8litekirin.mk	
